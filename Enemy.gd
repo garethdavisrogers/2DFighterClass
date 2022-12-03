294b7af4fb8f9e89658f11a6b51a2c7254cecf3c
Gareth <garethdavisrogers@gmail.com>
2022-12-03 12:48:14 -0500

    enemy seeking more reliably but needs attack protocol

--- Enemy.gd
+++ Enemy.gd
@@ -1,56 +1,43 @@
 extends "res://Fighter.gd"
 
-var attack_range = 100
+var in_melee_attack_range = false
 var target_position = null
-##Vector2 of player coordinates
 var player_detected = false
 ##player_detected is based on enter/exit $Detector signals
 
 func _physics_process(delta):
 	##enemy can only function if not staggered
-	if(stun_timer < 0):
-		if(player_detected):
-			state_machine('seek')
-		if(movedir.x != 0 or movedir.y != 0):
-			anim_switch('walk')
-		match state:
-			'seek':
-				state_seek()
-			'idle':
-				state_idle()
-			'attack':
-				state_attack('lite')
-			'stagger':
-				state_stagger()
-		if(cool_down > -1):
-			cool_down -= delta
-	else:
+	if(stun_timer > 0):
 		stun_timer -= delta
-	
+	if(player_detected == false):
+		state_machine('idle')
+	else:
+		state_machine('seek')
+	match state:
+		'attack':
+			state_attack('lite')
+		'stagger':
+			state_stagger()
+		'seek':
+			state_seek()
+		'idle':
+			state_idle()
+	if(cool_down > 0):
+		cool_down -= delta
+		
 func state_seek():
 	var t = get_player_position()
-	movement_loop()
-	spritedir_loop()
-	var s = self.get_position()
-	var x_distance = abs(s.x - t.x)
-	var y_distance = abs(s.y - t.y)
-	if(x_distance < attack_range and y_distance < attack_range and cool_down < 0):
-		var r = randi()%2 + 1
-		anim_switch(str('lite_attack', r))
-		cool_down = 2
-		state_machine('attack')
-	if(t.x < s.x and x_distance > attack_range):
-		movedir.x = -1
-	elif(t.x > s.x and x_distance > attack_range):
-		movedir.x = 1
+	##if no player is in detect radius, change to idle state
+	if(t != null):
+		in_melee_attack_range = get_is_in_melee_range(t)
+		if(in_melee_attack_range):
+			movedir = Vector2(0, 0)
+			state_machine('attack')
+		else:
+			close_distance(t)
 	else:
-		movedir.x = 0
-	if(t.y < s.y and y_distance > attack_range):
-		movedir.y = -1
-	elif(t.y > s.y and y_distance > attack_range):
-		movedir.y = 1
-	else:
-		movedir.y = 0
+		in_melee_attack_range = false
+		state_machine('idle')
 	
 func get_player_position():
 	var bodies = $Detector.get_overlapping_bodies()
@@ -58,6 +45,29 @@ func get_player_position():
 		if(body.type == 'player'):
 			return body.position
 
+func get_is_in_melee_range(player_pos):
+	var s = self.get_position()
+	var x_distance = abs(s.x - player_pos.x)
+	var y_distance = abs(s.y - player_pos.y)
+	if(x_distance < 100 and y_distance < 0):
+		return true
+	else:
+		return false
+
+func close_distance(player_pos):
+	var s = self.get_position()
+	if(player_pos.x > s.x):
+		movedir.x = 1
+	else:
+		movedir.x = -1
+	if(player_pos.y > s.y):
+		movedir.y = 1
+	else:
+		movedir.y = -1
+	movement_loop()
+	spritedir_loop()
+	anim_switch('walk')
+	
 func _on_Detector_body_entered(body):
 	if(body.type == 'player'):
 		player_detected = true
@@ -69,4 +79,5 @@ func _on_Detector_body_exited(_body):
 	for b in bodies:
 		if(b.type == 'player'):
 			return
-	state_machine('idle')
+	player_detected = false
+	movedir = Vector2(0, 0)
